119f77d06a0628540762a6bb769e4f38d788ab88
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2025-05-26 00:37:20 +0530

    jwt key testing

--- src/main/java/com/jobportal/jwt/JwtHelper.java
+++ src/main/java/com/jobportal/jwt/JwtHelper.java
@@ -1,5 +1,6 @@
 package com.jobportal.jwt;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,48 +16,54 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 // ChatGpt : Write jwt helper class for jwt authentication
-@Component                       
+@Component
 public class JwtHelper {
-	private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-	
+	// private static final Key SECRET_KEY =
+	// Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+	private static final String SECRET = "MySuperSecretKeyForJWTThatIsAtLeast32Bytes!"; // Must be 32+ characters
+	private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
 	private static final long JWT_TOKEN_VALIDITY = 3600000;
-	
+
 	public String getUsernameFromToken(String token) {
 		return getClaimFromToken(token, Claims::getSubject);
 	}
-	
+
 	public Date getExpirationDateFromToken(String token) {
 		return getClaimFromToken(token, Claims::getExpiration);
 	}
-	
+
 	public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
 		final Claims claims = getAllClaimsFromToken(token);
 		return claimsResolver.apply(claims);
 	}
-	
+
 	public Claims getAllClaimsFromToken(String token) {
 		return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getBody();
 	}
-	
+
 	private Boolean isTokenExpired(String token) {
 		final Date expiration = getExpirationDateFromToken(token);
 		return expiration.before(new Date());
 	}
-	
+
 	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<>();
-		CustomUserDetails customeUser = (CustomUserDetails)userDetails;
+		CustomUserDetails customeUser = (CustomUserDetails) userDetails;
 		claims.put("id", customeUser.getId());
 		claims.put("name", customeUser.getName());
 		claims.put("accountType", customeUser.getAccountType());
 		claims.put("profileId", customeUser.getProfileId());
 		return doGenerateToken(claims, userDetails.getUsername());
 	}
-	
+
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
-		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY)).signWith(SECRET_KEY).compact();
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY)).signWith(SECRET_KEY)
+				.compact();
 	}
-	
+
 	public Boolean validateToken(String token, String username) {
 		final String tokenUsername = getUsernameFromToken(token);
 		return (tokenUsername.equals(username) && !isTokenExpired(token));
